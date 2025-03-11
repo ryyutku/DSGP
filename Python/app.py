8cafea4335d5b8ba7c69a04e492e9cf24e2f15ef
ryyutku <anukim123@gmail.com>
2025-03-11 11:06:25 +0530

    script update

--- Python/app.py
+++ Python/app.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
 # Load the trained model and scaler
-model = pickle.load(open("fuel_demand_model.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 fuel_scaler = pickle.load(open("fuel_scaler.pkl", "rb"))
 
